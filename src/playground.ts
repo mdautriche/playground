9a15647902a23e1daad91b8e79fcb0415a760dd1
mdautriche <dautriche.e1606532@etud.univ-ubs.fr>
2018-11-30 09:56:13 +0100

    Version_Test_SVM_1
    - Ajout d'un fichier svm.ts contenant l'algorithme SVM.
    - Modification prototype --> classe dans le code SVM.
    - Version temporaire de Playground dédiée à SVM.
    - Ajout de fonctions permettant de formater les entrées/sorties afin de communiquer avec l'algorithme SVM.
    (dataset --> playground --> svm --> playground --> heatmp).

--- src/playground.ts
+++ src/playground.ts
@@ -27,6 +27,7 @@ import {
 } from "./state";
 import {Example2D, shuffle} from "./dataset";
 import {AppendingLineChart} from "./linechart";
+import {Svm} from "./svm";
 
 let mainWidth;
 
@@ -51,6 +52,9 @@ const BIAS_SIZE = 5;
 const NUM_SAMPLES_CLASSIFY = 500;
 const NUM_SAMPLES_REGRESS = 1200;
 const DENSITY = 100;
+
+let train: boolean = false;
+let svmA = new Svm();
 
 enum HoverType {
   BIAS, WEIGHT
@@ -381,7 +385,7 @@ function makeGUI() {
         .getBoundingClientRect().width;
     if (newWidth !== mainWidth) {
       mainWidth = newWidth;
-      drawNetwork(network);
+      //drawNetwork(network);
       updateUI(true);
     }
   });
@@ -394,405 +398,13 @@ function makeGUI() {
   }
 }
 
-function updateBiasesUI(network: nn.Node[][]) {
-  nn.forEachNode(network, true, node => {
-    d3.select(`rect#bias-${node.id}`).style("fill", colorScale(node.bias));
-  });
-}
-
-function updateWeightsUI(network: nn.Node[][], container: d3.Selection<any>) {
-  for (let layerIdx = 1; layerIdx < network.length; layerIdx++) {
-    let currentLayer = network[layerIdx];
-    // Update all the nodes in this layer.
-    for (let i = 0; i < currentLayer.length; i++) {
-      let node = currentLayer[i];
-      for (let j = 0; j < node.inputLinks.length; j++) {
-        let link = node.inputLinks[j];
-        container.select(`#link${link.source.id}-${link.dest.id}`)
-            .style({
-              "stroke-dashoffset": -iter / 3,
-              "stroke-width": linkWidthScale(Math.abs(link.weight)),
-              "stroke": colorScale(link.weight)
-            })
-            .datum(link);
-      }
-    }
-  }
-}
-
-function drawNode(cx: number, cy: number, nodeId: string, isInput: boolean,
-    container: d3.Selection<any>, node?: nn.Node) {
-  let x = cx - RECT_SIZE / 2;
-  let y = cy - RECT_SIZE / 2;
-
-  let nodeGroup = container.append("g")
-    .attr({
-      "class": "node",
-      "id": `node${nodeId}`,
-      "transform": `translate(${x},${y})`
-    });
-
-  // Draw the main rectangle.
-  nodeGroup.append("rect")
-    .attr({
-      x: 0,
-      y: 0,
-      width: RECT_SIZE,
-      height: RECT_SIZE,
-    });
-  let activeOrNotClass = state[nodeId] ? "active" : "inactive";
-  if (isInput) {
-    let label = INPUTS[nodeId].label != null ?
-        INPUTS[nodeId].label : nodeId;
-    // Draw the input label.
-    let text = nodeGroup.append("text").attr({
-      class: "main-label",
-      x: -10,
-      y: RECT_SIZE / 2, "text-anchor": "end"
-    });
-    if (/[_^]/.test(label)) {
-      let myRe = /(.*?)([_^])(.)/g;
-      let myArray;
-      let lastIndex;
-      while ((myArray = myRe.exec(label)) != null) {
-        lastIndex = myRe.lastIndex;
-        let prefix = myArray[1];
-        let sep = myArray[2];
-        let suffix = myArray[3];
-        if (prefix) {
-          text.append("tspan").text(prefix);
-        }
-        text.append("tspan")
-        .attr("baseline-shift", sep === "_" ? "sub" : "super")
-        .style("font-size", "9px")
-        .text(suffix);
-      }
-      if (label.substring(lastIndex)) {
-        text.append("tspan").text(label.substring(lastIndex));
-      }
-    } else {
-      text.append("tspan").text(label);
-    }
-    nodeGroup.classed(activeOrNotClass, true);
-  }
-  if (!isInput) {
-    // Draw the node's bias.
-    nodeGroup.append("rect")
-      .attr({
-        id: `bias-${nodeId}`,
-        x: -BIAS_SIZE - 2,
-        y: RECT_SIZE - BIAS_SIZE + 3,
-        width: BIAS_SIZE,
-        height: BIAS_SIZE,
-      }).on("mouseenter", function() {
-        updateHoverCard(HoverType.BIAS, node, d3.mouse(container.node()));
-      }).on("mouseleave", function() {
-        updateHoverCard(null);
-      });
-  }
-
-  // Draw the node's canvas.
-  let div = d3.select("#network").insert("div", ":first-child")
-    .attr({
-      "id": `canvas-${nodeId}`,
-      "class": "canvas"
-    })
-    .style({
-      position: "absolute",
-      left: `${x + 3}px`,
-      top: `${y + 3}px`
-    })
-    .on("mouseenter", function() {
-      selectedNodeId = nodeId;
-      div.classed("hovered", true);
-      nodeGroup.classed("hovered", true);
-      updateDecisionBoundary(network, false);
-      heatMap.updateBackground(boundary[nodeId], state.discretize);
-    })
-    .on("mouseleave", function() {
-      selectedNodeId = null;
-      div.classed("hovered", false);
-      nodeGroup.classed("hovered", false);
-      updateDecisionBoundary(network, false);
-      heatMap.updateBackground(boundary[nn.getOutputNode(network).id],
-          state.discretize);
-    });
-  if (isInput) {
-    div.on("click", function() {
-      state[nodeId] = !state[nodeId];
-      parametersChanged = true;
-      reset();
-    });
-    div.style("cursor", "pointer");
-  }
-  if (isInput) {
-    div.classed(activeOrNotClass, true);
-  }
-  let nodeHeatMap = new HeatMap(RECT_SIZE, DENSITY / 10, xDomain,
-      xDomain, div, {noSvg: true});
-  div.datum({heatmap: nodeHeatMap, id: nodeId});
-
-}
-
-// Draw network
-function drawNetwork(network: nn.Node[][]): void {
-  let svg = d3.select("#svg");
-  // Remove all svg elements.
-  svg.select("g.core").remove();
-  // Remove all div elements.
-  d3.select("#network").selectAll("div.canvas").remove();
-  d3.select("#network").selectAll("div.plus-minus-neurons").remove();
-
-  // Get the width of the svg container.
-  let padding = 3;
-  let co = d3.select(".column.output").node() as HTMLDivElement;
-  let cf = d3.select(".column.features").node() as HTMLDivElement;
-  let width = co.offsetLeft - cf.offsetLeft;
-  svg.attr("width", width);
-
-  // Map of all node coordinates.
-  let node2coord: {[id: string]: {cx: number, cy: number}} = {};
-  let container = svg.append("g")
-    .classed("core", true)
-    .attr("transform", `translate(${padding},${padding})`);
-  // Draw the network layer by layer.
-  let numLayers = network.length;
-  let featureWidth = 118;
-  let layerScale = d3.scale.ordinal<number, number>()
-      .domain(d3.range(1, numLayers - 1))
-      .rangePoints([featureWidth, width - RECT_SIZE], 0.7);
-  let nodeIndexScale = (nodeIndex: number) => nodeIndex * (RECT_SIZE + 25);
-
-
-  let calloutThumb = d3.select(".callout.thumbnail").style("display", "none");
-  let calloutWeights = d3.select(".callout.weights").style("display", "none");
-  let idWithCallout = null;
-  let targetIdWithCallout = null;
-
-  // Draw the input layer separately.
-  let cx = RECT_SIZE / 2 + 50;
-  let nodeIds = Object.keys(INPUTS);
-  let maxY = nodeIndexScale(nodeIds.length);
-  nodeIds.forEach((nodeId, i) => {
-    let cy = nodeIndexScale(i) + RECT_SIZE / 2;
-    node2coord[nodeId] = {cx, cy};
-    drawNode(cx, cy, nodeId, true, container);
-  });
-
-  // Draw the intermediate layers.
-  for (let layerIdx = 1; layerIdx < numLayers - 1; layerIdx++) {
-    let numNodes = network[layerIdx].length;
-    let cx = layerScale(layerIdx) + RECT_SIZE / 2;
-    maxY = Math.max(maxY, nodeIndexScale(numNodes));
-    addPlusMinusControl(layerScale(layerIdx), layerIdx);
-    for (let i = 0; i < numNodes; i++) {
-      let node = network[layerIdx][i];
-      let cy = nodeIndexScale(i) + RECT_SIZE / 2;
-      node2coord[node.id] = {cx, cy};
-      drawNode(cx, cy, node.id, false, container, node);
-
-      // Show callout to thumbnails.
-      let numNodes = network[layerIdx].length;
-      let nextNumNodes = network[layerIdx + 1].length;
-      if (idWithCallout == null &&
-          i === numNodes - 1 &&
-          nextNumNodes <= numNodes) {
-        calloutThumb.style({
-          display: null,
-          top: `${20 + 3 + cy}px`,
-          left: `${cx}px`
-        });
-        idWithCallout = node.id;
-      }
-
-      // Draw links.
-      for (let j = 0; j < node.inputLinks.length; j++) {
-        let link = node.inputLinks[j];
-        let path: SVGPathElement = drawLink(link, node2coord, network,
-            container, j === 0, j, node.inputLinks.length).node() as any;
-        // Show callout to weights.
-        let prevLayer = network[layerIdx - 1];
-        let lastNodePrevLayer = prevLayer[prevLayer.length - 1];
-        if (targetIdWithCallout == null &&
-            i === numNodes - 1 &&
-            link.source.id === lastNodePrevLayer.id &&
-            (link.source.id !== idWithCallout || numLayers <= 5) &&
-            link.dest.id !== idWithCallout &&
-            prevLayer.length >= numNodes) {
-          let midPoint = path.getPointAtLength(path.getTotalLength() * 0.7);
-          calloutWeights.style({
-            display: null,
-            top: `${midPoint.y + 5}px`,
-            left: `${midPoint.x + 3}px`
-          });
-          targetIdWithCallout = link.dest.id;
-        }
-      }
-    }
-  }
-
-  // Draw the output node separately.
-  cx = width + RECT_SIZE / 2;
-  let node = network[numLayers - 1][0];
-  let cy = nodeIndexScale(0) + RECT_SIZE / 2;
-  node2coord[node.id] = {cx, cy};
-  // Draw links.
-  for (let i = 0; i < node.inputLinks.length; i++) {
-    let link = node.inputLinks[i];
-    drawLink(link, node2coord, network, container, i === 0, i,
-        node.inputLinks.length);
-  }
-  // Adjust the height of the svg.
-  svg.attr("height", maxY);
-
-  // Adjust the height of the features column.
-  let height = Math.max(
-    getRelativeHeight(calloutThumb),
-    getRelativeHeight(calloutWeights),
-    getRelativeHeight(d3.select("#network"))
-  );
-  d3.select(".column.features").style("height", height + "px");
-}
-
-function getRelativeHeight(selection: d3.Selection<any>) {
-  let node = selection.node() as HTMLAnchorElement;
-  return node.offsetHeight + node.offsetTop;
-}
-
-function addPlusMinusControl(x: number, layerIdx: number) {
-  let div = d3.select("#network").append("div")
-    .classed("plus-minus-neurons", true)
-    .style("left", `${x - 10}px`);
-
-  let i = layerIdx - 1;
-  let firstRow = div.append("div").attr("class", `ui-numNodes${layerIdx}`);
-  firstRow.append("button")
-      .attr("class", "mdl-button mdl-js-button mdl-button--icon")
-      .on("click", () => {
-        let numNeurons = state.networkShape[i];
-        if (numNeurons >= 8) {
-          return;
-        }
-        state.networkShape[i]++;
-        parametersChanged = true;
-        reset();
-      })
-    .append("i")
-      .attr("class", "material-icons")
-      .text("add");
-
-  firstRow.append("button")
-      .attr("class", "mdl-button mdl-js-button mdl-button--icon")
-      .on("click", () => {
-        let numNeurons = state.networkShape[i];
-        if (numNeurons <= 1) {
-          return;
-        }
-        state.networkShape[i]--;
-        parametersChanged = true;
-        reset();
-      })
-    .append("i")
-      .attr("class", "material-icons")
-      .text("remove");
-
-  let suffix = state.networkShape[i] > 1 ? "s" : "";
-  div.append("div").text(
-    state.networkShape[i] + " neuron" + suffix
-  );
-}
-
-function updateHoverCard(type: HoverType, nodeOrLink?: nn.Node | nn.Link,
-    coordinates?: [number, number]) {
-  let hovercard = d3.select("#hovercard");
-  if (type == null) {
-    hovercard.style("display", "none");
-    d3.select("#svg").on("click", null);
-    return;
-  }
-  d3.select("#svg").on("click", () => {
-    hovercard.select(".value").style("display", "none");
-    let input = hovercard.select("input");
-    input.style("display", null);
-    input.on("input", function() {
-      if (this.value != null && this.value !== "") {
-        if (type === HoverType.WEIGHT) {
-          (nodeOrLink as nn.Link).weight = +this.value;
-        } else {
-          (nodeOrLink as nn.Node).bias = +this.value;
-        }
-        updateUI();
-      }
-    });
-    input.on("keypress", () => {
-      if ((d3.event as any).keyCode === 13) {
-        updateHoverCard(type, nodeOrLink, coordinates);
-      }
-    });
-    (input.node() as HTMLInputElement).focus();
-  });
-  let value = (type === HoverType.WEIGHT) ?
-    (nodeOrLink as nn.Link).weight :
-    (nodeOrLink as nn.Node).bias;
-  let name = (type === HoverType.WEIGHT) ? "Weight" : "Bias";
-  hovercard.style({
-    "left": `${coordinates[0] + 20}px`,
-    "top": `${coordinates[1]}px`,
-    "display": "block"
-  });
-  hovercard.select(".type").text(name);
-  hovercard.select(".value")
-    .style("display", null)
-    .text(value.toPrecision(2));
-  hovercard.select("input")
-    .property("value", value.toPrecision(2))
-    .style("display", "none");
-}
-
-function drawLink(
-    input: nn.Link, node2coord: {[id: string]: {cx: number, cy: number}},
-    network: nn.Node[][], container: d3.Selection<any>,
-    isFirst: boolean, index: number, length: number) {
-  let line = container.insert("path", ":first-child");
-  let source = node2coord[input.source.id];
-  let dest = node2coord[input.dest.id];
-  let datum = {
-    source: {
-      y: source.cx + RECT_SIZE / 2 + 2,
-      x: source.cy
-    },
-    target: {
-      y: dest.cx - RECT_SIZE / 2,
-      x: dest.cy + ((index - (length - 1) / 2) / length) * 12
-    }
-  };
-  let diagonal = d3.svg.diagonal().projection(d => [d.y, d.x]);
-  line.attr({
-    "marker-start": "url(#markerArrow)",
-    class: "link",
-    id: "link" + input.source.id + "-" + input.dest.id,
-    d: diagonal(datum, 0)
-  });
-
-  // Add an invisible thick link that will be used for
-  // showing the weight value on hover.
-  container.append("path")
-    .attr("d", diagonal(datum, 0))
-    .attr("class", "link-hover")
-    .on("mouseenter", function() {
-      updateHoverCard(HoverType.WEIGHT, input, d3.mouse(this));
-    }).on("mouseleave", function() {
-      updateHoverCard(null);
-    });
-  return line;
-}
-
 /**
  * Given a neural network, it asks the network for the output (prediction)
  * of every node in the network using inputs sampled on a square grid.
  * It returns a map where each key is the node ID and the value is a square
  * matrix of the outputs of the network for each input in the grid respectively.
  */
+
 function updateDecisionBoundary(network: nn.Node[][], firstTime: boolean) {
   if (firstTime) {
     boundary = {};
@@ -837,6 +449,7 @@ function updateDecisionBoundary(network: nn.Node[][], firstTime: boolean) {
   }
 }
 
+/*
 function getLoss(network: nn.Node[][], dataPoints: Example2D[]): number {
   let loss = 0;
   for (let i = 0; i < dataPoints.length; i++) {
@@ -847,24 +460,37 @@ function getLoss(network: nn.Node[][], dataPoints: Example2D[]): number {
   }
   return loss / dataPoints.length;
 }
-
+*/
 function updateUI(firstStep = false) {
-  // Update the links visually.
-  updateWeightsUI(network, d3.select("g.core"));
-  // Update the bias values visually.
-  updateBiasesUI(network);
-  // Get the decision boundary of the network.
-  updateDecisionBoundary(network, firstStep);
-  let selectedId = selectedNodeId != null ?
-      selectedNodeId : nn.getOutputNode(network).id;
-  heatMap.updateBackground(boundary[selectedId], state.discretize);
-
-  // Update all decision boundaries.
-  d3.select("#network").selectAll("div.canvas")
-      .each(function(data: {heatmap: HeatMap, id: string}) {
-    data.heatmap.updateBackground(reduceMatrix(boundary[data.id], 10),
-        state.discretize);
-  });
+  let heatmapSquare: [[number, number]] = [[0,0]];
+  let length: number = 100;
+  for (var i = 0; i < length; ++i) {
+    for (var k = 1; k < length; ++k) {
+      heatmapSquare.push([i,k]);
+    }
+  }
+  var marginsHeatmap: number[][] = new Array(100);
+  for (var i = 0; i < 100; ++i) {
+    marginsHeatmap[i] = new Array(100);
+  }
+  if(train){
+    var margins: number[] = svmA.margins();
+    let j = 0;
+    for (var i = 0; i < length; ++i) {
+      for (var k = 0; k < length; ++k) {
+        marginsHeatmap[i][k] = margins[j];
+        j++;
+      }
+    }
+  }else{
+    for (var i = 0; i < length; ++i) {
+      for (var k = 0; k < length; ++k) {
+        marginsHeatmap[i][k] = 1;
+      }
+    }
+  }
+  alert("updateUI Out");
+  heatMap.updateBackground(marginsHeatmap, state.discretize);
 
   function zeroPad(n: number): string {
     let pad = "000000";
@@ -907,18 +533,22 @@ function constructInput(x: number, y: number): number[] {
 }
 
 function oneStep(): void {
-  iter++;
-  trainData.forEach((point, i) => {
-    let input = constructInput(point.x, point.y);
-    nn.forwardProp(network, input);
-    nn.backProp(network, point.label, nn.Errors.SQUARE);
-    if ((i + 1) % state.batchSize === 0) {
-      nn.updateWeights(network, state.learningRate, state.regularizationRate);
-    }
-  });
-  // Compute the loss.
-  lossTrain = getLoss(network, trainData);
-  lossTest = getLoss(network, testData);
+  //alert("OneStep Inside");
+  let trainDataSvm: [[number, number]] = [[0,0]];
+  let k: number = 0;
+  for (var i of trainData) {
+      trainDataSvm.push([i.x, i.y]);
+  }
+  //alert("After first loop");
+  k = 0;
+  let trainLabels: number[] = [];
+  for (var i of trainData) {
+      trainLabels.push(i.label);
+      k++;
+  }
+  svmA.train(trainDataSvm, trainLabels, {C: 1.0});
+  train = true;
+  alert("OneStep Out");
   updateUI();
 }
 
@@ -957,9 +587,9 @@ function reset(onStartup=false) {
       nn.Activations.LINEAR : nn.Activations.TANH;
   network = nn.buildNetwork(shape, state.activation, outputActivation,
       state.regularization, constructInputIds(), state.initZero);
-  lossTrain = getLoss(network, trainData);
-  lossTest = getLoss(network, testData);
-  drawNetwork(network);
+  //lossTrain = getLoss(network, trainData);
+  //lossTest = getLoss(network, testData);
+  //drawNetwork(network);
   updateUI(true);
 };
 
